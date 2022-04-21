151ad33ec347f73de116cdaef5184edd3fd0da16
amrutasadalgi <45632489+amrutasadalgi@users.noreply.github.com>
2022-04-21 15:37:05 +0530

    Add files via upload

---
+++ mystery/img/index.js
@@ -0,0 +1,31 @@
+// let str = "java";
+// let str1 = "scripta";
+// for (let i = 0; i < str.length; i++)
+// {
+
+//     for (let j = 0; j < str1.length; j++)
+//     {
+//         if (str[i] == str1[j]) {
+//             console.log("stopped");
+//         }
+            
+//     }
+// }
+
+
+
+let str = "java";
+let str2 = "scripting language";
+for (let i = 0; i < str.length; i++){
+    for (let j = 0; j < str2.length; j++)
+    {
+        if (str[i] == str[j])
+        {
+            console.log(str[i]);
+            }
+        }
+}
+
+let strr = "kumar";
+let strrr = "selva";
+console.log(strrr,strr)
